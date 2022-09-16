42f03af0aa1e96d6664e35f003c44d18bb094261
David Fischer <85546373+fischer-hub@users.noreply.github.com>
2022-09-16 18:41:48 +0200

    remove sqalchemy

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from sqlalchemy import true
 from webdriver_manager.chrome import ChromeDriverManager
 
 
